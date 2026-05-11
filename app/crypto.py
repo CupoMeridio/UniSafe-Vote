01ecd857ae422466ba77b702722efec2f2e86bc3
Vittorio Postiglione <32437140+CupoMeridio@users.noreply.github.com>
2026-05-11 21:06:05 +0200

    Update crypto.py

--- app/crypto.py
+++ app/crypto.py
@@ -1,1 +1,6 @@
-
+from cryptography.hazmat.primitives import hashes
+# Qui metteremo le funzioni per:
+# - Generare coppie di chiavi
+# - Cifrare il voto
+# - Firmare i messaggi (per l'Autenticità)
+# - Calcolare l'hash per la ricevuta (per la Verificabilità)
