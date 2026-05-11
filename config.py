5f98c5cbf03450a30c991543d6638e7bd38b592c
Vittorio Postiglione <32437140+CupoMeridio@users.noreply.github.com>
2026-05-11 21:06:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'default-key-for-dev')
+    # Se DATABASE_URL non esiste (locale), usa SQLite
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///unisafevote.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
